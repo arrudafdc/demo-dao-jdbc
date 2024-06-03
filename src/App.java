bf2adb1293fe4db39411f6c63f305bfab84283ad
arrudafdc <arrudafdc@gmail.com>
2024-06-03 11:11:58 -0300

    Department entity class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import model.entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
