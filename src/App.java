0c468392bc651399ea08849d9d058e2553406bdf
arrudafdc <arrudafdc@gmail.com>
2024-06-04 19:21:16 -0300

    findAll implementation

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
-import db.DB;
+
+
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
@@ -7,10 +10,12 @@ import model.entities.Seller;
 public class App {
     public static void main(String[] args) throws Exception {
 
-       SellerDao sellerDao = DaoFactory.createSellerDao();
-       Seller seller = sellerDao.findById(3);
-       System.out.println(seller);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        List<Seller> list = sellerDao.findAll();
 
-       DB.closeConnection();
+        for (Seller seller : list) {
+            System.out.println(seller);
+        }
+
     }
 }
