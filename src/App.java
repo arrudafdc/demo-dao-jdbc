62a76f3fc0d28682aa79aa8d3904d7e9e595cd5e
arrudafdc <arrudafdc@gmail.com>
2024-06-04 22:11:39 -0300

    update implementation

--- src/App.java
+++ src/App.java
@@ -1,22 +1,22 @@
-
-
-import java.sql.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
+
 
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(8);
 
-        Seller seller = new Seller(null, "João Silva", "joao@gmail.com", new Date(0), 15000.00, new Department(1, null));
+        seller.setName("Bruno Henrique");
+        seller.setEmail("bruno@gmail.com");
+        seller.setBaseSalary(9000.00);
 
-        sellerDao.insert(seller);
-        System.out.println(seller);
+        sellerDao.update(seller);
 
     }
 }
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -70,7 +70,30 @@ public class SellerDaoJdbc implements SellerDao {
 
   @Override
   public void update(Seller obj) {
-    // TODO Auto-generated method stub
+    PreparedStatement st = null;
+    try {
+      st = conn.prepareStatement(
+        "UPDATE seller "
+        + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+        + "WHERE Id = ?"
+      );
+
+      st.setString(1, obj.getName());
+      st.setString(2, obj.getEmail());
+      st.setDate(3, new Date(obj.getBirthDate().getTime()));
+      st.setDouble(4, obj.getBaseSalary());
+      st.setInt(5, obj.getDepartment().getId());
+      st.setInt(6, obj.getId());
+
+      st.executeUpdate();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    finally {
+      DB.closeStatement(st);
+    }
+
     
   }
 
